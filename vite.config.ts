cee792f05a03c721eb426db19d942a07958dcc54
Ritik Kumar Pathak <phritik06@gmail.com>
2024-11-04 12:15:37 +0530

    deploy logs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT || '3000'),
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: parseInt(process.env.PORT || '3000'),
+    host: '0.0.0.0'
+  }
 })
