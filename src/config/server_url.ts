49976cb21c2bdaa5935d67ae6995bd3d41ea25cd
Ritik Kumar Pathak <phritik06@gmail.com>
2024-11-08 20:20:21 +0530

    minor fixes

--- src/config/server_url.ts
+++ src/config/server_url.ts
@@ -1,5 +1,5 @@
-export const SERVER_BASE_API = "http://localhost:8000"
-// export const SERVER_BASE_API = "https://sparkmate-backend-production.up.railway.app"
+// export const SERVER_BASE_API = "http://localhost:8000"
+export const SERVER_BASE_API = "https://sparkmate-backend-production.up.railway.app"
 // export const SERVER_BASE_API = "https://spartmate-backend-2.onrender.com";
 
 // urls: [
